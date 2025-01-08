db58a96a6a92279fe86beb4f3e880078d1a48654
Jude Errol Gamboa <judeerrol.gamboa@gmail.com>
2025-01-08 19:22:26 +0800

    Update active_sessions_controller.rb

--- app/controllers/active_sessions_controller.rb
+++ app/controllers/active_sessions_controller.rb
@@ -2,7 +2,7 @@ class ActiveSessionsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    # Test change #2
+    # Test change #3
     @active_session = current_user.active_sessions.find(params[:id])
 
     @active_session.destroy
